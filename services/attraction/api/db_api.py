e80f46a32c450707238d1f3833b3bce5379fa36f
Yicong M. Gong <gongyc3@gmail.com>
2018-03-08 13:34:15 -0800

    updated attraction related db_api and attraction

--- services/attraction/api/db_api.py
+++ services/attraction/api/db_api.py
@@ -5,3 +5,52 @@ def test():
     response = requests.get(DB_ENDPOINT)
     data = response.json()
     return data
+
+class attraction_post():
+
+    def __init__(self, name, lat, lng, 
+            intro, rating, cover_file, marker_file,
+            user_id):
+        self.name = name
+        self.lat = lat
+        self.lng = lng
+        self.intro = intro
+        self.rating = rating
+        self.cover_file = cover_file
+        self.marker_file = marker_file
+        self.user_id = user_id
+
+    def push(self):
+        
+        url = DB_ENDPOINT + 'attraction_post' # define end-point
+
+        data = {
+            'name': self.name,
+            'lat':  self.lat,
+            'lng':  self.lng,
+            'intro':self.intro,
+            'rating':self.rating,
+            'user_id': self.user_id
+        }
+
+        files = {'cover': self.cover_file, 'marker': self.marker_file}
+
+        response = requests.post(url, files = files, data = data)
+
+        return response
+
+class place_look_up_object():
+    
+    def __init__(self, ID):
+        self.ID = ID
+
+    def get(self):
+        
+        url = DB_ENDPOINT + 'place_lookup' # define end-point
+
+        params = {'ID': self.ID}
+        data = requests.get(url, params = params)
+        place_data = data['place']
+        review_data = data['review']
+
+        return (place_data, review_data)
--- services/attraction/attraction.py
+++ services/attraction/attraction.py
@@ -1,6 +1,8 @@
 # Dummy Attraction
 # All class name default as service
 from services.attraction.api import db_api
+from django.shortcuts import render
+
 class service():
 
     def __init__(self):
@@ -13,11 +15,22 @@ class service():
         data = db_api.test()
         return data
 
-    def look_up_place_data_and_render(self):
-        pass
+    def look_up_place_data_and_render(self, ID):
+        obj = db_api.place_look_up_object(ID)
+        place_info, reviews_data = obj.get()
+        reviews_data = reviews_data[0:-1]
+        context = {'place_info': place_info, 'reviews_data': reviews_data}
+        return render(None, 'services/attraction/templates/place.html', context)
 
     def post_attraction(self, name, lat, lng, 
             intro, rating, cover_file, marker_file,
             user_id
         ):
-        pass
+        obj = db_api.attraction_post(name, lat, lng, 
+            intro, rating, cover_file, marker_file,
+            user_id)
+
+        return obj.push()
+
+if __name__ == '__main__':
+    pass
