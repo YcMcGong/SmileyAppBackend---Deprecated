f0f68288a50ae8d45d9b7dbdffad9dc6b4ab6c98
Yicong M. Gong <gongyc3@gmail.com>
2018-03-13 15:17:08 -0700

    Updated attraction service

--- services/attraction/api/db_api.py
+++ services/attraction/api/db_api.py
@@ -6,11 +6,16 @@ def test():
     data = response.json()
     return data
 
+"""
+#  ________________________________________
+# |Attraction                              |
+# |________________________________________|
+"""
+# Post attraction
 class attraction_post():
 
-    def __init__(self, name, lat, lng, 
-            intro, rating, cover_file, marker_file,
-            user_id):
+    def __init__(self, name, lat, lng, intro, rating, cover_file, marker_file,
+            user_id, if_custom = False):
         self.name = name
         self.lat = lat
         self.lng = lng
@@ -19,8 +24,9 @@ class attraction_post():
         self.cover_file = cover_file
         self.marker_file = marker_file
         self.user_id = user_id
+        self.if_custom = if_custom
 
-    def push(self):
+    def post(self):
         
         url = DB_ENDPOINT + 'attraction_post' # define end-point
 
@@ -30,7 +36,8 @@ class attraction_post():
             'lng':  self.lng,
             'intro':self.intro,
             'rating':self.rating,
-            'user_id': self.user_id
+            'user_id': self.user_id,
+            'if_custom': self.if_custom
         }
 
         files = {'cover': self.cover_file, 'marker': self.marker_file}
@@ -39,7 +46,104 @@ class attraction_post():
 
         return response
 
-class place_look_up_object():
+# Get attraction by ID, or, coordinate, or address
+class attraction_get():
+
+    def __init__(self, ID = None, lat = None, lng = None, address = None):
+        self.ID = ID
+        self.lat = lat
+        self.lng = lng
+        self.address = address
+
+    def get(self):
+        
+        # Get by id
+        if self.ID:
+            url = DB_ENDPOINT + 'attraction_get_by_id' # define end-point
+            params = {'ID': self.ID}
+
+        # Get by coordinate
+        elif self.lat and self.lng:
+            url = DB_ENDPOINT + 'attraction_get_by_coordinate' # define end-point
+            params = {'lat': self.lat, 'lng': self.lng}
+
+        # Get by address
+        elif self.address:
+            url = DB_ENDPOINT + 'attraction_get_by_address' # define end-point
+            params = {'lat': self.address}
+
+        # No input
+        else:
+            return None
+
+        data = requests.get(url, params = params) # send http request
+
+        return data
+
+
+"""
+#  ________________________________________
+# |Attraction List                         |
+# |________________________________________|
+"""
+
+# Get a list of attraction using a jsonified list of users
+class attraction_list_get():
+
+    def __init__(self, user_list_json):
+        self.user_list_json = user_list_json
+
+    def post(self):
+        
+        url = DB_ENDPOINT + 'attraction_post' # define end-point
+
+        params = {'user_list': self.user_list_json}
+
+        data = requests.get(url, params = params) # send http request
+
+        return data
+
+
+"""
+#  ________________________________________
+# |Review                                  |
+# |________________________________________|
+"""
+
+class review_post():
+    
+    def __init__(self, intro, rating, cover_file, user_id, attraction_id):
+
+        self.intro = intro
+        self.rating = rating
+        self.cover_file = cover_file
+        self.user_id = user_id
+        self.attraction_id = attraction_id
+
+    def post(self):
+        
+        url = DB_ENDPOINT + 'review_post' # define end-point
+
+        data = {
+            'intro': self.intro,
+            'rating':self.rating,
+            'user_id': self.user_id,
+            'attraction_id':self.attraction_id
+        }
+
+        files = {'cover': self.cover_file}
+
+        response = requests.post(url, files = files, data = data)
+
+        return response
+
+"""
+#  ________________________________________
+# |Attraction Details                      |
+# |________________________________________|
+"""
+
+class attraction_detail_object():
     
     def __init__(self, ID):
         self.ID = ID
---
+++ services/attraction/utility.py
@@ -0,0 +1,6 @@
+# Provide utility functions for attraction service
+
+def resize_to_marker(image):
+
+    output_image = image
+    return output_image
